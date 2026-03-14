230c848ff4d31de44eb5e177dbc64b5765e7cbfa
Aayush Raj <devxpriyanshu@gmail.com>
2026-03-14 22:03:48 +0530

    enabling caching

--- src/main/java/com/ecom/trial/TrialApplication.java
+++ src/main/java/com/ecom/trial/TrialApplication.java
@@ -1,10 +1,11 @@
-
 package com.ecom.trial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TrialApplication {
 
 	public static void main(String[] args) {
